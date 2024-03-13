9be282a9591b08a486f6d0527d1b9461de1a589c
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-13 17:27:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
-# Raw content URL for an Excel file
+import streamlit as st
+import pandas as pd
+
+# Replace this URL with the correct raw content URL for your Excel file on GitHub
 github_excel_url = 'https://github.com/Badam1666/Elections/raw/main/elections_geo_dpt.xlsx'
 
 # Read Excel file into a DataFrame
 df = pd.read_excel(github_excel_url)
 
-print(df.info())
+# Display the DataFrame in Streamlit
+st.dataframe(df)
