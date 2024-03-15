aba01ab88c2dfc689484fcbbf1f45b45dc31aec8
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 08:14:41 +0100

    Update page1.py

--- app/pages/page1.py
+++ app/pages/page1.py
@@ -1,11 +1,3 @@
 import streamlit as st
 
-def main():
-    st.set_page_config(page_title="Orientations Politiques")
-    
-    # Your Streamlit app content goes here
-    st.title("Welcome to Orientations Politiques")
-    st.write("This is the main content of the page.")
-
-if __name__ == "__main__":
-    main()
+st.set_page_config(page_title = "Orientations Politiques")
