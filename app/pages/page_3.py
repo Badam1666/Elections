032ecb867a70a718fb7d7c2d81b04a263c3d192b
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:29:25 +0100

    Update page_3.py

--- app/pages/page_3.py
+++ app/pages/page_3.py
@@ -1,37 +1,39 @@
 import streamlit as st
 st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-def show_page_4():
-    st.title("Commune")
-    st.write("Contenu de la page 4")
-    # Add more content or Streamlit components as needed for page 4
+
 
 def show_page_3():
     st.title("Carte")
     st.write("Contenu de la page 3")
     # Add more content or Streamlit components as needed for page 3
 
-import streamlit as st
-from datetime import datetime, timedelta
+import time
+from datetime import datetime, timezone, timedelta
 
-def countdown_timer(target_date):
-    current_time = datetime.now()
-    time_remaining = target_date - current_time
-    days = time_remaining.days
-    hours, remainder = divmod(time_remaining.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return days, hours, minutes, seconds
+def count_down(ts):
+    with st.empty():
+        while ts:
+            current_time = datetime.now(timezone.utc) + timedelta(hours=1)  # UTC+1 time
+            if current_time >= datetime(2024, 5, 1, 0, 0, tzinfo=timezone.utc):
+                st.header("Fin des inscriptions en ligne")
+                break
+            
+            mins, secs = divmod(ts, 60)
+            time_now = '{:02d}:{:02d}'.format(mins, secs)
+            st.header(f"{time_now}")
+            time.sleep(1)
+            ts -= 1
+        else:
+            st.header("Time Up!")
 
 def main():
-    st.title("Countdown to May 1st, 00:00 AM (UTC+1)")
-    target_date = datetime(year=2024, month=5, day=1, hour=0, minute=0, second=0) + timedelta(hours=1)  # Adjusting for UTC+1
+    st.title("Countdown Timer")
+    time_minutes = st.number_input('Enter the time in minutes ', min_value=1, value=25)
+    time_in_seconds = time_minutes * 60
+    
+    if st.button("START"):
+        count_down(int(time_in_seconds))
 
-    while True:
-        days, hours, minutes, seconds = countdown_timer(target_date)
-        st.write(f"Time Remaining: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
-        if days == 0 and hours == 0 and minutes == 0 and seconds == 0:
-            st.write("Countdown complete!")
-            break
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
