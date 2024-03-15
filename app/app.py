1b1606cad288044358c5fe93fd3ce0421ce82d5a
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:53:56 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,1 @@
 import streamlit as st
-
-def main():
-    # Hide the current page (app.py) from the sidebar
-    st.experimental_set_query_params(page=None)
-
-    st.title("Elections Européennes")
-    st.write("Bienvenue sur notre application sur les élections européennes!")
-
-if __name__ == "__main__":
-    main()
