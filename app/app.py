e8d5b5f8f6e609e5adb59e954109d72c8f5593ff
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 15:54:31 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -33,8 +33,8 @@ def main():
     st.write("3. **Résultats par Commune** : Consultez les résultats des élections européennes de 2014 et 2019 dans votre commune, vous donnant un aperçu détaillé de la participation et des votes pour chaque parti politique.")
     st.write("Notre objectif est de rendre les élections européennes accessibles à tous, afin que chacun puisse prendre des décisions éclairées et participer pleinement à la démocratie européenne.")
 
-    # Nos prévisions
-    st.header("Nos Prévisions")
+    # Nos prédictions
+    st.header("Nos Prédictions")
     st.write("Nous sommes des étudiants à Le Wagon, une école de codage renommée, et nous avons entrepris une démarche de collecte de données auprès de Data gouv pour mener nos recherches. Notre objectif est de rendre les élections européennes accessibles à tous, afin que chacun puisse comprendre les enjeux et participer activement à la vie démocratique.")
     st.write("Pour prédire les élections de 2024, nous avons utilisé des algorithmes de machine learning, une approche moderne qui nous a permis d'analyser les tendances passées et de faire des projections pour l'avenir. Cependant, l'algorithme ne prend pas en compte le climat politique et social et a ses limites dans la prédiction de futures élections. Vous trouverez ci-dessous les prévisions de Nostradamus, notre plateforme de prédiction, comparées aux sondages Ipsos réalisés au début du mois de Mars.")
 
@@ -58,77 +58,74 @@ def main():
         
         time.sleep(1)
 
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
+    st.title("Comparaison Prédictions et Sondages")
+    st.write("Nous comparons ici les prédictions de Nostradamus aux résultats des sondages Ipsos.")
+    st.write("Vous pouvez voir ci-dessous les prévisions de Nostradamus pour les élections de 2024, ainsi que les résultats des sondages Ipsos réalisés au début du mois de Mars.")
 
-# Load data
-url = "https://github.com/AliciaD31/elections-nostradamus/blob/main/predictions2024_Python.csv?raw=true"
-df = pd.read_csv(url)
+    # Load data
+    url = "https://github.com/AliciaD31/elections-nostradamus/blob/main/predictions2024_Python.csv?raw=true"
+    df = pd.read_csv(url)
 
-# Renomme les orientations politiques
-df['Orientation politique'] = df['Orientation politique'].replace({
-    'extreme_gauche': 'Extrême gauche',
-    'gauche': 'Gauche',
-    'centre_gauche': 'Centre gauche',
-    'centre': 'Centre',
-    'centre_droite': 'Centre droite',
-    'droite': 'Droite',
-    'extreme_droite': 'Extrême droite',
-    'divers': 'Divers'
-})
+    # Renomme les orientations politiques
+    df['Orientation politique'] = df['Orientation politique'].replace({
+        'extreme_gauche': 'Extrême gauche',
+        'gauche': 'Gauche',
+        'centre_gauche': 'Centre gauche',
+        'centre': 'Centre',
+        'centre_droite': 'Centre droite',
+        'droite': 'Droite',
+        'extreme_droite': 'Extrême droite',
+        'divers': 'Divers'
+    })
 
-# Define colors for each category
-colors = {
-    'Divers': '#EEEEEE',
-    'Extrême droite': '#242F7F',
-    'Droite': '#0066CC',
-    'Centre droite': '#82A2C6',
-    'Centre': '#ffcc00ff',
-    'Centre gauche': '#F3D79A',
-    'Gauche': '#FF8080',
-    'Extrême gauche': '#BB0000'
-}
+    # Define colors for each category
+    colors = {
+        'Divers': '#EEEEEE',
+        'Extrême droite': '#242F7F',
+        'Droite': '#0066CC',
+        'Centre droite': '#82A2C6',
+        'Centre': '#ffcc00ff',
+        'Centre gauche': '#F3D79A',
+        'Gauche': '#FF8080',
+        'Extrême gauche': '#BB0000'
+    }
 
-# Sort dataframe by the percentages
-df_sorted_sondages = df.sort_values(by='Sondages_2024', ascending=True)
-df_sorted = df.sort_values(by='predictions_2024', ascending=True)
+    # Sort dataframe by the percentages
+    df_sorted_sondages = df.sort_values(by='Sondages_2024', ascending=True)
+    df_sorted = df.sort_values(by='predictions_2024', ascending=True)
 
-# Create a Streamlit app
-st.title('Election Predictions')
+    # Display the predictions and sondages plots side by side
+    col1, col2 = st.columns(2)
 
-# Display the predictions and sondages plots side by side
-col1, col2 = st.columns(2)
+    # Plot for predictions_2024
+    with col1:
+        fig1, ax1 = plt.subplots(figsize=(8, 8))
+        ax1.barh(df_sorted['Orientation politique'], df_sorted['predictions_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted['Orientation politique']])
+        ax1.set_title('Prédictions Nostradamus')
+        ax1.set_xlabel('Pourcentage de votes')
+        ax1.set_ylabel('Orientation politique')
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['bottom'].set_visible(False)
+        ax1.spines['left'].set_visible(False)
+        for index, value in enumerate(df_sorted['predictions_2024']):
+            ax1.text(value, index, f'{value}%', va='center')
+        st.pyplot(fig1)
 
-# Plot for predictions_2024
-with col1:
-    fig1, ax1 = plt.subplots(figsize=(8, 8))
-    ax1.barh(df_sorted['Orientation politique'], df_sorted['predictions_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted['Orientation politique']])
-    ax1.set_title('Prédictions Nostradamus')
-    ax1.set_xlabel('Pourcentage de votes')
-    ax1.set_ylabel('Orientation politique')
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['bottom'].set_visible(False)
-    ax1.spines['left'].set_visible(False)
-    for index, value in enumerate(df_sorted['predictions_2024']):
-        ax1.text(value, index, f'{value}%', va='center')
-    st.pyplot(fig1)
-
-# Plot for Sondages_2024
-with col2:
-    fig2, ax2 = plt.subplots(figsize=(8, 8))
-    ax2.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
-    ax2.set_title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
-    ax2.set_xlabel('Pourcentage de votes')
-    ax2.set_ylabel('Orientation politique')
-    ax2.spines['top'].set_visible(False)
-    ax2.spines['right'].set_visible(False)
-    ax2.spines['bottom'].set_visible(False)
-    ax2.spines['left'].set_visible(False)
-    for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
-        ax2.text(value, index, f'{value}%', va='center')
-    st.pyplot(fig2)
+    # Plot for Sondages_2024
+    with col2:
+        fig2, ax2 = plt.subplots(figsize=(8, 8))
+        ax2.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
+        ax2.set_title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
+        ax2.set_xlabel('Pourcentage de votes')
+        ax2.set_ylabel('Orientation politique')
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['bottom'].set_visible(False)
+        ax2.spines['left'].set_visible(False)
+        for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
+            ax2.text(value, index, f'{value}%', va='center')
+        st.pyplot(fig2)
 
 if __name__ == '__main__':
     main()
