fae649f181899147211acc920fdadd0ed3472fd0
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:47:57 +0100

    Update home.py

--- app/pages/home.py
+++ app/pages/home.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-def show_home_page():
-    st.title("Accueil")
-    st.write("Bienvenue sur notre application sur les élections européennes!")
-    # Add more content or Streamlit components as needed for your home page
 
 import time
 from datetime import datetime, timezone, timedelta
