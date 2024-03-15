725cf48e773ca36e7e5d8ba29b4ec62b186efbfc
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 12:20:07 +0100

    Update page_1.py

--- app/pages/page_1.py
+++ app/pages/page_1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 def main():
     st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
