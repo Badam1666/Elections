04e3d232d06bc48a704a4adcfac4d97aa0dc55a0
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 18:53:39 +0100

    Update 2 Communes.py

--- app/pages/2 Communes.py
+++ app/pages/2 Communes.py
@@ -1,43 +1,32 @@
-import pandas as pd
+import streamlit as st
+st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# Load data
 
-def load_data():
-    url = "https://raw.githubusercontent.com/Badam1666/Elections/main/raw_data/Elections_Communes_Final.csv"
-    data = pd.read_csv(url)
-    return data
 
-data = load_data()
 
-# Function to calculate percentage difference
-def calculate_percentage_diff(year1, year2):
-    return ((year2 - year1) / year1) * 100
+import time
+from datetime import datetime, timezone, timedelta
 
-# Streamlit app
-st.title('Election Data Explorer')
+def count_down(target_datetime):
+    with st.empty():
+        while datetime.now(timezone.utc) < target_datetime:
+            remaining_time = target_datetime - datetime.now(timezone.utc)
+            days, seconds = divmod(remaining_time.total_seconds(), 86400)
+            hours, seconds = divmod(seconds, 3600)
+            minutes, seconds = divmod(seconds, 60)
+            
+            time_remaining = f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds"
+            
+            st.header(f"{time_remaining}")
+            time.sleep(1)
+        
+        st.header("Fin des inscriptions en ligne")
 
-# Search bar for commune selection
-search_query = st.text_input('Search for a commune (code or name):').lower()
+def main():
+    st.title("Countdown to May 1st, 2024")
+    target_datetime = datetime(2024, 5, 1, 0, 0, tzinfo=timezone.utc)
+    
+    count_down(target_datetime)
 
-# Filter data based on search query
-filtered_data = data[data['libelle_commune'].str.lower().str.contains(search_query)]
-
-if not filtered_data.empty:
-    # Display filtered data
-    st.subheader('Election Data for Selected Commune(s)')
-    st.write(filtered_data)
-
-    # Calculate percentage difference for selected commune(s)
-    years = filtered_data['annee'].unique()
-    if len(years) == 2:
-        year1_data = filtered_data[filtered_data['annee'] == years[0]].iloc[:, 2:]
-        year2_data = filtered_data[filtered_data['annee'] == years[1]].iloc[:, 2:]
-        percentage_diff = calculate_percentage_diff(year1_data, year2_data)
-
-        st.subheader('Percentage Difference Between Years')
-        st.write(percentage_diff)
-    else:
-        st.write('Please select a commune with data for both 2014 and 2019.')
-
-else:
-    st.write('No data available for the selected commune.')
+if __name__ == '__main__':
+    main()
