105bb052bf18099e7a5f1ddc67b354d3155a2c50
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 12:58:43 +0100

    Update Accueil.py

--- app/Accueil.py
+++ app/Accueil.py
@@ -8,10 +8,10 @@ add_page_title()
 # and icons should be
 show_pages(
     [
-        Page("pages/home.py", "Accueil", "🏠"),
-        Page("pages/page_2.py", "Orientations politiques", "📊"),
-        Page("pages/page_3.py", "Carte", "🗺️"),
-        Page("pages/page_4.py", "Commune", "🏘️"),
+        Page("https://github.com/Badam1666/Elections/blob/main/app/pages/home.py", "Accueil", "🏠"),
+        Page("https://github.com/Badam1666/Elections/blob/main/app/pages/page_2.py", "Orientations politiques", "📊"),
+        Page("https://github.com/Badam1666/Elections/blob/main/app/pages/page_3.py", "Carte", "🗺️"),
+        Page("https://github.com/Badam1666/Elections/blob/main/app/pages/page_4.py", "Commune", "🏘️"),
     ]
 )
 
@@ -21,12 +21,7 @@ def main():
 
     st.title("Elections Européennes")
     st.write("Bienvenue sur notre application sur les élections européennes!")
-    
-    st.header("Liens vers les pages:")
-    st.markdown("- [Accueil](https://github.com/Badam1666/Elections/blob/main/app/pages/home.py)")
-    st.markdown("- [Orientations politiques](https://github.com/Badam1666/Elections/blob/main/app/pages/page_2.py)")
-    st.markdown("- [Carte](https://github.com/Badam1666/Elections/blob/main/app/pages/page_3.py)")
-    st.markdown("- [Commune](https://github.com/Badam1666/Elections/blob/main/app/pages/page_4.py)")
 
 if __name__ == "__main__":
     main()
+
