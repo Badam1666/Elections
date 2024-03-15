2c68a1d6daf95f3353660907f878cd3631389df3
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 16:59:28 +0100

    Update Accueil.py

--- app/Accueil.py
+++ app/Accueil.py
@@ -1,72 +1,109 @@
 import streamlit as st
+import time
+from datetime import datetime, timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Load data
-@st.cache
-def load_data():
-    url = "https://raw.githubusercontent.com/Badam1666/Elections/main/raw_data/Elections_Communes_Final.csv"
-    data = pd.read_csv(url)
-    return data
+def main():
+    st.title("Bienvenue sur le site de Nostradamus")
+    st.subheader("Les élections européennes à votre portée")
 
-data = load_data()
+    # Explication des élections européennes
+    st.write("Les prochaines élections européennes auront lieu du 6 au 9 juin 2024. Vous ne savez pas trop comment ces élections fonctionnent et quels sont leurs enjeux ? Pas d'inquiétude, la team Nostradamus vous aide à y voir plus clair !")
+    st.write("- Tous les cinq ans, les citoyens des États membres de l'Union européenne votent pour élire leurs représentants au Parlement européen.")
+    st.write("- Chaque État membre a un nombre de sièges proportionnel à sa population.")
+    st.write("- Les résultats des élections européennes impactent directement le quotidien des citoyens de l'Union européenne. C'est grâce au Parlement européen que des décisions cruciales pour notre environnement ont été prises, telles que l'interdiction des plastiques à usage unique. De même, le règlement général sur la protection des données (RGPD), adopté par le Parlement européen, renforce nos droits fondamentaux à la vie privée et à la sécurité des données dans un monde de plus en plus numérisé.")
+    st.write("Chaque vote aux élections européennes compte pour façonner un avenir plus durable, juste et sûr pour tous les citoyens européens !")
 
-# Streamlit app
-st.title('Répartition des votes par commune')
-st.subheader('Recherchez votre commune dans la barre de gauche pour afficher les résultats des années 2014 et 2019')
 
-# Multiselect widget for commune selection
-options = data['libelle_commune'].str.lower().unique()
-selected_communes = st.sidebar.multiselect('Sélectionnez votre commune(s):', options)
+    # Box bleue avec lien pour vérifier le statut électoral
+    st.sidebar.markdown("<div style='background-color: #4169E1; padding: 8px; border-radius: 5px; margin-bottom: 10px;'>"
+                        "<a style='color: white; text-decoration: none;' href='https://www.service-public.fr/particuliers/vosdroits/demarches-et-outils/ISE'>Vérifiez votre statut électoral !</a>"
+                        "</div>", unsafe_allow_html=True)
+    
+    # Box jaune avec countdown
+    st.sidebar.markdown("<div style='background-color: #FFD700; padding: 8px; border-radius: 5px;'>"
+                        "<div style='color: black; font-size: small;'>"
+                        "<h3>Temps restant avant la fin des inscriptions en ligne :</h3>"
+                        "</div>"
+                        "</div>", unsafe_allow_html=True)
 
-# Filter data based on selected communes
-filtered_data = data[data['libelle_commune'].str.lower().isin(selected_communes)]
+    # Sur ce site
+    st.subheader("Sur ce site")
+    st.write("Bienvenue sur le site de Nostradamus, votre source d'informations essentielles sur les élections européennes. Sur notre plateforme, vous trouverez :")
+    st.write("1. **Carte des Résultats** : Explorez une carte interactive des résultats des élections européennes par département afin de visualiser les tendances politiques à travers la France depuis 2004.")
+    st.write("2. **Résultats par Commune** : Consultez les résultats des élections européennes de 2014 et 2019 dans votre commune, vous donnant un aperçu détaillé de la participation et des votes pour chaque parti politique.")
+    st.write("Notre objectif est de rendre les élections européennes accessibles à tous, afin que chacun puisse prendre des décisions éclairées et participer pleinement à la démocratie européenne.")
 
-if not filtered_data.empty:
-    # Display filtered data
-    st.subheader('Données électorales pour la ou les communes sélectionnées')
+    # Nos prédictions
+    st.subheader("Nos prédictions")
+    st.write("Nous sommes des étudiants en Data Analyse à Le Wagon, nous avons entrepris une démarche de collecte de données auprès de Data gouv pour mener nos recherches. Notre objectif est de rendre les élections européennes accessibles à tous, afin que chacun puisse comprendre les enjeux et participer activement à la vie démocratique.")
+    st.write("Pour prédire les résultats des élections de 2024, nous avons utilisé des algorithmes de machine learning, une technologie d'intelligence artificielle qui nous a permis d'analyser les tendances passées et de faire des projections pour 2024. L'algorithme présente quelques limites dans le sens où il ne prend pas en compte le climat politique et social actuel. Vous trouverez ci-dessous nos prévisions que nous avons comparées au sondage Ipsos réalisé au début du mois de mars 2024.")
 
-    for commune in selected_communes:
-        st.subheader(f'{commune}')
+    # Load data
+    url = "https://github.com/AliciaD31/elections-nostradamus/blob/main/predictions2024_Python.csv?raw=true"
+    df = pd.read_csv(url)
 
-        # Filter data for the current commune
-        commune_data = filtered_data[filtered_data['libelle_commune'].str.lower() == commune.lower()]
-        
-        # Create a dictionary to store party percentages for each year
-        party_percentages = {}
-        for year in [2014, 2019]:
-            year_data = commune_data[commune_data['annee'] == year]
-            if not year_data.empty:
-                total_votes = year_data.iloc[0][2:].sum()  # Assuming the vote columns start from 3rd column
-                party_percentages[year] = {party: (year_data.iloc[0][party] / total_votes) * 100 if total_votes != 0 else 0 for party in ['extreme_gauche', 'gauche', 'centre_gauche', 'centre', 'centre_droite', 'droite', 'extreme_droite']}
-            else:
-                party_percentages[year] = {}
-        
-        # Create DataFrame from party percentages
-        party_df = pd.DataFrame.from_dict(party_percentages).transpose()
-        party_df = party_df.rename(columns={party: f'{party}_percentage' for party in party_df.columns})
-        
-        # Add year columns to the DataFrame
-        party_df['2014'] = party_df.index.map(lambda x: x['extreme_gauche_percentage'] if 'extreme_gauche_percentage' in x else 0)
-        party_df['2019'] = party_df.index.map(lambda x: x['extreme_gauche_percentage'] if 'extreme_gauche_percentage' in x else 0)
-        
-        # Compute total votes, null votes, and winner party
-        total_votes = commune_data['votants'].sum()
-        null_votes = commune_data['null'].sum()
-        winner_party = commune_data.iloc[0]['Tête']
-        
-        # Create a DataFrame for additional information
-        info_df = pd.DataFrame({
-            'Métrique': ['Total des votes', 'Votes nuls/blancs', 'Parti gagnant'],
-            'Valeur': [total_votes, null_votes, winner_party]
-        })
-        
-        # Display party percentages, votants, null, and additional information
-        st.write('Répartition des votes par parti:')
-        st.write(party_df)
-        st.write('Votes (votants) et nuls/blancs:')
-        st.write(commune_data[['libelle_commune', 'votants', 'null']])
-        st.write('Informations supplémentaires:')
-        st.write(info_df)
+    # Renomme les orientations politiques
+    df['Orientation politique'] = df['Orientation politique'].replace({
+        'extreme_gauche': 'Extrême gauche',
+        'gauche': 'Gauche',
+        'centre_gauche': 'Centre gauche',
+        'centre': 'Centre',
+        'centre_droite': 'Centre droite',
+        'droite': 'Droite',
+        'extreme_droite': 'Extrême droite',
+        'divers': 'Divers'
+    })
 
-else:
-    st.write('Aucune donnée disponible pour la ou les communes sélectionnées.')
+    # Define colors for each category
+    colors = {
+        'Divers': '#EEEEEE',
+        'Extrême droite': '#242F7F',
+        'Droite': '#0066CC',
+        'Centre droite': '#82A2C6',
+        'Centre': '#ffcc00ff',
+        'Centre gauche': '#F3D79A',
+        'Gauche': '#FF8080',
+        'Extrême gauche': '#BB0000'
+    }
+
+    # Sort dataframe by the percentages
+    df_sorted_sondages = df.sort_values(by='Sondages_2024', ascending=True)
+    df_sorted = df.sort_values(by='predictions_2024', ascending=True)
+
+    # Display the predictions and sondages plots side by side
+    col1, col2 = st.columns(2)
+
+    # Plot for predictions_2024
+    with col1:
+        fig1, ax1 = plt.subplots(figsize=(8, 8))
+        ax1.barh(df_sorted['Orientation politique'], df_sorted['predictions_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted['Orientation politique']])
+        ax1.set_title('Prédictions Nostradamus')
+        ax1.set_xlabel('Pourcentage de votes')
+        ax1.set_ylabel('Orientation politique')
+        ax1.spines['top'].set_visible(False)
+        ax1.spines['right'].set_visible(False)
+        ax1.spines['bottom'].set_visible(False)
+        ax1.spines['left'].set_visible(False)
+        for index, value in enumerate(df_sorted['predictions_2024']):
+            ax1.text(value, index, f'{value}%', va='center')
+        st.pyplot(fig1)
+
+    # Plot for Sondages_2024
+    with col2:
+        fig2, ax2 = plt.subplots(figsize=(8, 8))
+        ax2.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
+        ax2.set_title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
+        ax2.set_xlabel('Pourcentage de votes')
+        ax2.set_ylabel('Orientation politique')
+        ax2.spines['top'].set_visible(False)
+        ax2.spines['right'].set_visible(False)
+        ax2.spines['bottom'].set_visible(False)
+        ax2.spines['left'].set_visible(False)
+        for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
+            ax2.text(value, index, f'{value}%', va='center')
+        st.pyplot(fig2)
+
+if __name__ == '__main__':
+    main()
