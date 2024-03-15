8afbfd225c352b33c5436d854aafdc97aa4c408a
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 15:28:39 +0100

    Update 1 Orientations Politiques.py

--- app/pages/1 Orientations Politiques.py
+++ app/pages/1 Orientations Politiques.py
@@ -40,24 +40,8 @@ st.title('Election Predictions')
 # Display the predictions and sondages plots side by side
 col1, col2 = st.columns(2)
 
-# Plot for Sondages_2024
-with col1:
-    plt.figure(figsize=(8, 8))
-    plt.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
-    plt.title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
-    plt.xlabel('Pourcentage de votes')
-    plt.ylabel('Orientation politique')
-    plt.gca().spines['top'].set_visible(False)
-    plt.gca().spines['right'].set_visible(False)
-    plt.gca().spines['bottom'].set_visible(False)
-    plt.gca().spines['left'].set_visible(False)
-    for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
-        plt.text(value, index, f'{value}%', va='center')
-    fig1 = plt.gcf()
-    st.pyplot(fig1)
-
 # Plot for predictions_2024
-with col2:
+with col1:
     plt.figure(figsize=(8, 8))
     plt.barh(df_sorted['Orientation politique'], df_sorted['predictions_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted['Orientation politique']])
     plt.title('Prédictions Nostradamus')
@@ -69,5 +53,19 @@ with col2:
     plt.gca().spines['left'].set_visible(False)
     for index, value in enumerate(df_sorted['predictions_2024']):
         plt.text(value, index, f'{value}%', va='center')
-    fig2 = plt.gcf()
-    st.pyplot(fig2)
+    st.pyplot()
+
+# Plot for Sondages_2024
+with col2:
+    plt.figure(figsize=(8, 8))
+    plt.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
+    plt.title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
+    plt.xlabel('Pourcentage de votes')
+    plt.ylabel('Orientation politique')
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['bottom'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
+        plt.text(value, index, f'{value}%', va='center')
+    st.pyplot()
