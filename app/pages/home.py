c5deea66c745f9e6ce162d62985e4cf3dbee1731
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:20:05 +0100

    Update home.py

--- app/pages/home.py
+++ app/pages/home.py
@@ -1,23 +1,30 @@
 import streamlit as st
-st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
+from st_pages import Page, show_pages, add_page_title
+from pages.home import show_home_page
+from pages.page_2 import show_page_2
+from pages.page_3 import show_page_3
+from pages.page_4 import show_page_4
 
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+show_pages(
+    [
+        Page(show_home_page, "Accueil", "🏠"),
+        Page(show_page_2, "Orientations politiques", "📊"),
+        Page(show_page_3, "Carte", "🗺️"),
+        Page(show_page_4, "Commune", "🏘️"),
+    ]
+)
 
 def main():
-    st.title("Projet Nostradamus : Analyse des dernières élections européennes et prédiction pour 2024 🔮")
-    st.write("Les élections européennes sont l’occasion pour les citoyens de participer activement à la vie démocratique de l’Union européenne et d’influencer les décisions qui impactent leur quotidien. Alors, allons voter !")
-    st.write("Allez vérifier votre inscription sur ce lien : [Lien vers le service-public.fr](https://www.service-public.fr/particuliers/vosdroits/demarches-et-outils/ISE)")
+    # Hide the current page (app.py) from the sidebar
+    st.experimental_set_query_params(page=None)
 
-    st.header("Projet de 2 semaines dans le cadre de la formation du Wagon en data analytics")
-    st.write("Comprendre les élections européennes et leurs enjeux")
-    st.write("Contexte électoral français")
-    st.write("Décryptage des votes : Comprendre les tendances électorales")
-    st.write("Étude de cas : Focus sur la Haute Garonne")
-    st.write("Prédictions pour les élections européennes de 2024")
-
-    st.write("N’oublions pas que ces prédictions sont basées sur des tendances actuelles et peuvent évoluer. Restons attentifs aux développements politiques et aux changements sociodémographiques pour affiner nos prévisions.")
+    st.title("Elections Européennes")
+    st.write("Bienvenue sur notre application sur les élections européennes!")
 
 if __name__ == "__main__":
     main()
-
-if st.button("Carte"):
-    st.switch_page("pages/page2.py")
