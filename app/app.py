8e8f1f4caef3c3c5ed10177406cabffef4a30c0a
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 14:22:30 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -32,31 +32,8 @@ def main():
 import streamlit as st
 from datetime import datetime
 
-
 # Countdown jusqu'au 1er Mai à minuit
-target_datetime = datetime(datetime.now().year, 5, 1, 0, 0)
 
-st.sidebar.markdown("<div style='background-color: #FFFF00; padding: 10px; border-radius: 5px;'>"
-                    "<div style='color: black;'>"
-                    "<h3>Temps restant avant la fin des inscriptions en ligne</h3>"
-                    "</div>"
-                    "</div>", unsafe_allow_html=True)
-
-countdown_placeholder = st.sidebar.empty()
-
-while datetime.now() < target_datetime:
-    remaining_time = target_datetime - datetime.now()
-    days = remaining_time.days
-    hours, remainder = divmod(remaining_time.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    time_remaining = f"{days} jours, {hours} heures, {minutes} minutes, {seconds} secondes"
-    
-    countdown_placeholder.markdown("<div style='background-color: #FFFF00; padding: 10px; border-radius: 5px;'>"
-                                   "<div style='color: black;'>"
-                                   "<h3>Temps restant avant la fin des inscriptions en ligne</h3>"
-                                   f"<p>{time_remaining}</p>"
-                                   "</div>"
-                                   "</div>", unsafe_allow_html=True)
 
 
 
