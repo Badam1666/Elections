9694252dae8fcd8e450e77c5028c9eae912244c8
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 18:15:40 +0100

    Update Accueil.py

--- app/Accueil.py
+++ app/Accueil.py
@@ -1,8 +1,5 @@
 import streamlit as st
-import time
 from datetime import datetime, timedelta
-import pandas as pd
-import matplotlib.pyplot as plt
 
 def main():
     st.title("Bienvenue sur le site de Nostradamus")
@@ -19,21 +16,29 @@ def main():
     st.sidebar.markdown("<div style='background-color: #4169E1; padding: 8px; border-radius: 5px; margin-bottom: 10px;'>"
                         "<a style='color: white; text-decoration: none;' href='https://www.service-public.fr/particuliers/vosdroits/demarches-et-outils/ISE'>Vérifiez votre statut électoral !</a>"
                         "</div>", unsafe_allow_html=True)
-    
+
     # Countdown timer until 1st of May 2024
     target_date = datetime(2024, 5, 1)
-    current_date = datetime.now()
-    remaining_time = target_date - current_date
-    days_remaining = remaining_time.days
-    hours_remaining = remaining_time.seconds // 3600
-    minutes_remaining = (remaining_time.seconds % 3600) // 60
-    seconds_remaining = remaining_time.seconds % 60
+    remaining_time = target_date - datetime.now()
 
-    st.sidebar.markdown("<div style='background-color: #FFD700; padding: 8px; border-radius: 5px;'>"
-                        "<div style='color: black; font-size: small;'>"
-                        f"<h3>Temps restant jusqu'au 1er mai 2024 : {days_remaining} jours {hours_remaining} heures {minutes_remaining} minutes {seconds_remaining} secondes</h3>"
-                        "</div>"
-                        "</div>", unsafe_allow_html=True)
+    countdown_placeholder = st.sidebar.empty()
+
+    while remaining_time.total_seconds() > 0:
+        days = remaining_time.days
+        hours, remainder = divmod(remaining_time.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        countdown_placeholder.markdown(f"<div style='background-color: #FFD700; padding: 8px; border-radius: 5px;'>"
+                                      "<div style='color: black; font-size: small;'>"
+                                      f"<h3>Temps restant jusqu'au 1er mai 2024 : {days} jours {hours} heures {minutes} minutes {seconds} secondes</h3>"
+                                      "</div>"
+                                      "</div>", unsafe_allow_html=True)
+        remaining_time = target_date - datetime.now()
+
+    countdown_placeholder.markdown("<div style='background-color: #FFD700; padding: 8px; border-radius: 5px;'>"
+                                  "<div style='color: black; font-size: small;'>"
+                                  "<h3>Le temps est écoulé !</h3>"
+                                  "</div>"
+                                  "</div>", unsafe_allow_html=True)
 
     # Sur ce site
     st.subheader("Sur ce site")
@@ -45,76 +50,70 @@ def main():
     # Répartition
     st.subheader("Répartition par orientation politique")
     st.write("Durant les 20 dernières annnées d'élections européennes, les noms des partis et nuances politiques ont changé considérablement. Pour pouvoir être cohérent dans nos analyses et dans nos prédictions, nous avons décidé de les classer par orientation politique. Nous avons utilisé le site internet de chacun de ces partis et les informations de l'Assemblée Nationale pour effectuer ce tri.")
-    
+
     # Nos prédictions
     st.subheader("Nos prédictions")
     st.write("Nous sommes des étudiants en Data Analyse à Le Wagon, nous avons entrepris une démarche de collecte de données auprès de Data gouv pour mener nos recherches. Notre objectif est de rendre les élections européennes accessibles à tous, afin que chacun puisse comprendre les enjeux et participer activement à la vie démocratique.")
-    st.write("Pour prédire les résultats des élections de 2024, nous avons utilisé des algorithmes de machine learning, une technologie d'intelligence artificielle qui nous a permis d'analyser les tendances passées et de faire des projections pour 2024. L'algorithme présente quelques limites dans le sens où il ne prend pas en compte le climat politique et social actuel. Vous trouverez ci-dessous nos prévisions que nous avons comparées au sondage Ipsos réalisé au début du mois de mars 2024.")
 
-    # Load data
-    url = "https://github.com/AliciaD31/elections-nostradamus/blob/main/predictions2024_Python.csv?raw=true"
-    df = pd.read_csv(url)
-
-    # Renomme les orientations politiques
-    df['Orientation politique'] = df['Orientation politique'].replace({
-        'extreme_gauche': 'Extrême gauche',
-        'gauche': 'Gauche',
-        'centre_gauche': 'Centre gauche',
-        'centre': 'Centre',
-        'centre_droite': 'Centre droite',
-        'droite': 'Droite',
-        'extreme_droite': 'Extrême droite',
-        'divers': 'Divers'
-    })
-
-    # Define colors for each category
-    colors = {
-        'Divers': '#EEEEEE',
-        'Extrême droite': '#242F7F',
-        'Droite': '#0066CC',
-        'Centre droite': '#82A2C6',
-        'Centre': '#ffcc00ff',
-        'Centre gauche': '#F3D79A',
-        'Gauche': '#FF8080',
-        'Extrême gauche': '#BB0000'
+    # Répartition par année et orientation politique
+    st.subheader("Répartition par année et orientation politique")
+    data = {
+        2004: {
+            'Extrême gauche': "LPC, LXG",
+            'Gauche': "LPS, LDG",
+            'Centre gauche': "LEC, LVE",
+            'Centre': "-",
+            'Centre droite': "LUDF",
+            'Droite': "LUMP, LCP, LDD",
+            'Extrême droite': "LFN, LXD",
+            'Divers': "LDV, LRG"
+        },
+        2009: {
+            'Extrême gauche': "LEXG, LCOP",
+            'Gauche': "LSOC, LDVD",
+            'Centre gauche': "LVEC",
+            'Centre': "LCMD",
+            'Centre droite': "-",
+            'Droite': "LMAJ, LDVD",
+            'Extrême droite': "LFN, LEXD",
+            'Divers': "LAUT, LREG"
+        },
+        2014: {
+            'Extrême gauche': "LEXG, LFG",
+            'Gauche': "LDVG, LUG",
+            'Centre gauche': "LVEC",
+            'Centre': "LUC",
+            'Centre droite': "-",
+            'Droite': "LDVD, LUMP",
+            'Extrême droite': "LFN, LEXD",
+            'Divers': "LDIV"
+        },
+        2019: {
+            'Extrême gauche': "La France Insoumise, L'Europe des gens",
+            'Gauche': "Liste citoyenne",
+            'Centre gauche': "Europe Ecologie, Envie d'Europe, Urgence Ecologie",
+            'Centre': "Renaissance",
+            'Centre droite': "Union Droite Centre, Les européens",
+            'Droite': "-",
+            'Extrême droite': "Prenez le pouvoir, Debout ! La France, Ensemble pour le Frexit",
+            'Divers': "Parti animaliste"
+        },
+        2024: {
+            'Extrême gauche': "La France Insoumise, Lutte ouvrière, NPA, PCF",
+            'Gauche': "Parti socialiste et Place Publique",
+            'Centre gauche': "Europe Ecologie les Verts,Parti radical de Gauche",
+            'Centre': "Renaissance, Ecologie au centre",
+            'Centre droite': "Alliance rurale",
+            'Droite': "Les republicains, Notre Europe",
+            'Extrême droite': "Debout ! La France, Reconquête, Rassemblement national, Union populaire republicaine",
+            'Divers': "Parti animaliste"
+        }
     }
 
-    # Sort dataframe by the percentages
-    df_sorted_sondages = df.sort_values(by='Sondages_2024', ascending=True)
-    df_sorted = df.sort_values(by='predictions_2024', ascending=True)
-
-    # Display the predictions and sondages plots side by side
-    col1, col2 = st.columns(2)
-
-    # Plot for predictions_2024
-    with col1:
-        fig1, ax1 = plt.subplots(figsize=(8, 8))
-        ax1.barh(df_sorted['Orientation politique'], df_sorted['predictions_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted['Orientation politique']])
-        ax1.set_title('Prédictions Nostradamus')
-        ax1.set_xlabel('Pourcentage de votes')
-        ax1.set_ylabel('Orientation politique')
-        ax1.spines['top'].set_visible(False)
-        ax1.spines['right'].set_visible(False)
-        ax1.spines['bottom'].set_visible(False)
-        ax1.spines['left'].set_visible(False)
-        for index, value in enumerate(df_sorted['predictions_2024']):
-            ax1.text(value, index, f'{value}%', va='center')
-        st.pyplot(fig1)
-
-    # Plot for Sondages_2024
-    with col2:
-        fig2, ax2 = plt.subplots(figsize=(8, 8))
-        ax2.barh(df_sorted_sondages['Orientation politique'], df_sorted_sondages['Sondages_2024'], color=[colors.get(x, '#FFFFFF') for x in df_sorted_sondages['Orientation politique']])
-        ax2.set_title('Sondages Ipsos - 1-6 mars 2024 - 5169 répondants')
-        ax2.set_xlabel('Pourcentage de votes')
-        ax2.set_ylabel('Orientation politique')
-        ax2.spines['top'].set_visible(False)
-        ax2.spines['right'].set_visible(False)
-        ax2.spines['bottom'].set_visible(False)
-        ax2.spines['left'].set_visible(False)
-        for index, value in enumerate(df_sorted_sondages['Sondages_2024']):
-            ax2.text(value, index, f'{value}%', va='center')
-        st.pyplot(fig2)
+    for year, parties in data.items():
+        st.write(f"Année {year} :")
+        for party, votes in parties.items():
+            st.write(f"- {party} : {votes}")
 
 if __name__ == '__main__':
     main()
