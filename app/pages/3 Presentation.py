7e68750cc60f794a24058079a8fc7ff6ce0f72dd
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 21:38:09 +0100

    Update 3 Presentation.py

--- app/pages/3 Presentation.py
+++ app/pages/3 Presentation.py
@@ -1,5 +1,4 @@
-import streamlit as st
-from streamlit_googleslide import st_googleslide
-
-# Embed Google Slides presentation
-st_googleslide("1zKvvjRn7vbbdqqKRBzoQ6MjihuqhPbQgAGjBSxsGNc0")
+File "/home/adminuser/venv/lib/python3.9/site-packages/streamlit/runtime/scriptrunner/script_runner.py", line 535, in _run_script
+    exec(code, module.__dict__)
+File "/mount/src/elections/app/pages/3 Presentation.py", line 2, in <module>
+    from streamlit_googleslide import st_googleslide
