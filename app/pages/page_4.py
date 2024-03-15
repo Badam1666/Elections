7eef7dd672275d3465b788af5830538ff430e578
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:29:55 +0100

    Update page_4.py

--- app/pages/page_4.py
+++ app/pages/page_4.py
@@ -1,5 +1,6 @@
-
 import streamlit as st
+
+st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 def show_page_4():
     st.title("Commune")
