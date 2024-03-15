816a262ee073fec36602ba3780914db9504279ef
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 13:48:51 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,23 @@
 import streamlit as st
+from st_pages import Page, show_pages, add_page_title
+from pages.home import show_home_page
+from pages.page_2 import show_page_2
+from pages.page_3 import show_page_3
+from pages.page_4 import show_page_4
+
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles 
+# and icons should be
+show_pages(
+    [
+        Page(show_home_page, "Accueil", "🏠"),
+        Page(show_page_2, "Orientations politiques", "📊"),
+        Page(show_page_3, "Carte", "🗺️"),
+        Page(show_page_4, "Commune", "🏘️"),
+    ]
+)
 
 def main():
     # Hide the current page (app.py) from the sidebar
