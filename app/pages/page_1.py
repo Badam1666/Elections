74842a69d381d6eeb82348451f59d6ad9eb6d546
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 12:20:21 +0100

    Update page_1.py

--- app/pages/page_1.py
+++ app/pages/page_1.py
@@ -1,14 +1,2 @@
 import streamlit as st
 st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
-
-def main():
-    st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
-    
-    # Customizing the sidebar with the desired name
-    st.sidebar.title("Orientations Politiques")
-    
-    # Your Streamlit app content goes here
-    st.write("This is the main content of the page.")
-
-if __name__ == "__main__":
-    main()
