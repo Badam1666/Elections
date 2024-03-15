cea1698fdb2866fc851dfdf49509187a3d4ad714
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-15 11:18:06 +0100

    Update Accueil.py

--- app/Accueil.py
+++ app/Accueil.py
@@ -1,21 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Elections européennes", page_icon="🗳️", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
-# pages.py
-
-# Import necessary Streamlit module(s)
-import streamlit as st
-
-
-# Define the page dictionary
-pages_dict = {
-    "🏠 Accueil (Home)": accueil_page,
-    "📜 Orientations Politiques (Political Orientations)": orientations_politiques_page,
-    "🗺️ Carte (Map)": carte_page,
-    "🏙️ Commune (Municipality)": commune_page,
-    "🔮 Prédiction (Prediction)": prediction_page
-}
-
 
 def main():
     st.title("Projet Nostradamus : Analyse des dernières élections européennes et prédiction pour 2024 🔮")
