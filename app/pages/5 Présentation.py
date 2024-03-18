2c33b427a93cd2976ae7874607fd28e80b86fbe8
Adam B <127580466+Badam1666@users.noreply.github.com>
2024-03-18 15:38:17 +0100

    Update and rename 5 Presentation.py to 5 Présentation.py

--- app/pages/5 Présentation.py
+++ app/pages/5 Présentation.py
@@ -6,7 +6,7 @@ st.title("Présentation du 15 Mars 2024")
 # Embed Google Slides presentation using an <iframe> tag
 st.markdown(
     """
-    <iframe src="https://docs.google.com/presentation/d/1zKvvjRn7vbbdqqKRBzoQ6MjihuqhPbQgAGjBSxsGNc0/embed?start=false&loop=false&delayms=3000" 
+    <iframe src="https://docs.google.com/presentation/d/1CDn0vaTbuFKRuoe_XfFyOeAXoB32wWgLhOSFy8i8f3g/embed?start=false&loop=false&delayms=3000" 
     width="100%" height="600" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
     """,
     unsafe_allow_html=True
